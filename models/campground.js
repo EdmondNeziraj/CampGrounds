75a6dd98fca912dc09a4280071fb289e08a684bd
EdmondNeziraj <edmondneziraj10@gmail.com>
2022-11-11 07:03:48 -0500

    Developed review section

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,32 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
-const CampgroundSchema = new Schema ({
+const CampgroundSchema = new Schema({
     title: String,
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
+
+CampgroundSchema.post('findOneAndDelete', async function (campground) {
+    if(campground) {
+        for (let i = 0; i < campground.reviews.length; i++) {
+            console.log(campground.reviews[i].body);
+        }
+        await Review.remove({
+            _id: {
+                $in: campground.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
